e387ca9c689943b624a59e966b2474ce749a11f5
Marc Stroebel <mdstroebel@gmail.com>
2024-01-17 15:56:17 +1100

    release: 3.0.1

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ContextualSDK",
-            url:"https://repo.contextu.al/artifactory/contextual/ios/3.0.0/ContextualSDK.xcframework.zip",
-            checksum: "772bc9cbcad9ec0d1b3f36cd2f8d0e907da3ed62b1920188caef7497e0d9aa45")
+            url:"https://repo.contextu.al/artifactory/contextual/ios/3.0.1/ContextualSDK.xcframework.zip",
+            checksum: "c41176b2d3c1533815acb6a970d26a11207829dae3e0bf2ba7244421d5ab827b")
     ]
 )
